379e88a0f372368437bb4d83a936e3d26d305e76
V-L-A-P-P <86523933+V-L-A-P-P@users.noreply.github.com>
2022-04-13 22:03:44 +0300

    6
    1. Поменял формулы
    2. Добавил некоторые комментарии
    3. Поменял название методов для подсчёта формул
    4. Добавил возможность ввода данных через gui(Пока без проверки)

--- MyWin.py
+++ MyWin.py
@@ -14,7 +14,7 @@ class MyWin(QtWidgets.QMainWindow):
         QtWidgets.QWidget.__init__(self, parent)
         self.ui = SimulatorGUI.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.start_botton.clicked.connect(self.start_btn_clicked)
+        self.ui.start_button.clicked.connect(self.start_btn_clicked)
         '''
         with open("styles_dict.json", "r") as read_file:
             self.styles_dict = json.load(read_file)
@@ -24,8 +24,15 @@ class MyWin(QtWidgets.QMainWindow):
         # print(float(self.ui.k1_input.text()))
         # GraphBuilder.GraphBuilder.build_animated_graph([0])
         try:
+            self.statistic_calculator.coeffs_dict['k1'] = float(self.ui.k1_input.text())
+            self.statistic_calculator.coeffs_dict['k2'] = float(self.ui.k2_input.text())
+            self.statistic_calculator.coeffs_dict['k3'] = float(self.ui.k3_input.text())
+            self.statistic_calculator.coeffs_dict['k4'] = float(self.ui.k4_input.text())
+            self.statistic_calculator.coeffs_dict['k5'] = float(self.ui.k5_input.text())
+            self.statistic_calculator.coeffs_dict['k6'] = float(self.ui.k6_input.text())
+            self.statistic_calculator.coeffs_dict['k7'] = float(self.ui.k7_input.text())
             GraphBuilder.GraphBuilder.build_animated_graphs(
-                self.statistic_calculator.get_disease_stat_array([100000, 0, 0, 0, 0, 0], 300), 100, 300)
+                self.statistic_calculator.get_disease_stat_array([int(self.ui.h1_input.text()), int(self.ui.h2_input.text()), int(self.ui.s1_input.text()), int(self.ui.s2_input.text()), int(self.ui.d_input.text())], 300), 100, 300)
             print(traceback.format_exc())
         except Exception as _ex:
             print(traceback.format_exc())
--- StatisticCalculator.py
+++ StatisticCalculator.py
@@ -1,84 +1,72 @@
 class StatisticCalculator:
     def __init__(self):
-        self.coefs_dict = {  # словарь коэффицентов
-            'k1': 0.02,
-            # Коэфицент перехода из группы не вакцинированных в группу впервые заболевшие
-            'k2': 0.02,
-            # Коэфицент перехода из группы "Впервые заболевшие" в группу "Выздоровевшие"
-            'k3': 0.02,  # доля повторно заболевших людей
-            'k4': 0.02,  # доля выздоровевших людей после повторного заболевания
-            'k5': 0.02,  # доля вакцинированных людей, заболевших впервые
-            'k6': 0.02,  # доля умерших людей
-            'v': 0.02  # доля людей, которые прошли вакцинацию
+        self.coeffs_dict = {  # словарь коэффицентов
+            'k1': 0.02,  # Коэффицент перехода из группы "Здоровые невакцинированные" в группу "Больные невакцинированные"
+            'k2': 0.02,  # Коэффицент перехода из группы "Больные невакцинированные" в группу "Здоровые невакцинированные"
+            'k3': 0.02,  # Коэффицент перехода из группы "Здоровые вакцинированные" в группу "Больные вакцинированные"
+            'k4': 0.02,  # Коэффицент перехода из группы "Больные вакцинированные" в группу "Здоровые вакцинированные"
+            'k5': 0.02,  # Коэффицент перехода из группы "Здоровые невакцинированные" в группу "Здоровые вакцинированные"
+            'k6': 0.02,  # Коэффицент перехода из группы "Больные невакцинированные" в группу "Умершие"
+            'k7': 0.02  # Коэффицент перехода из группы "Больные вакцинированные" в группу "Умершие"
         }
 
-    def n1(self, n1_prev):  # формула расчёта количества не болевших и не привитых людей
-        return n1_prev \
-               - round(self.coefs_dict.get('k1') * n1_prev) \
-               - round(self.coefs_dict.get('v') * n1_prev)
+    def calculate_h1(self, h1_prev, s1_prev):  # формула расчёта количества не болевших и не привитых людей
+        return h1_prev \
+               + round(self.coeffs_dict.get('k2') * s1_prev) \
+               - round(self.coeffs_dict.get('k5') * h1_prev) \
+               - round(self.coeffs_dict.get('k1') * h1_prev)
 
-    def n2(self, n1_prev, n2_prev, n5_prev):  # формула расчёта количества заболевших впервые людей
-        return n2_prev \
-               + round(self.coefs_dict.get('k1') * n1_prev) \
-               - round(self.coefs_dict.get('k2') * n2_prev) \
-               + round(self.coefs_dict.get('k5') * n5_prev) \
-               - round(self.coefs_dict.get('k6') * n2_prev)
+    def calculate_h2(self, h1_prev, h2_prev, s2_prev):  # формула расчёта количества заболевших впервые людей
+        return h2_prev \
+               + round(self.coeffs_dict.get('k5') * h1_prev) \
+               + round(self.coeffs_dict.get('k4') * s2_prev) \
+               - round(self.coeffs_dict.get('k3') * h2_prev)
 
-    def n3(self, n2_prev, n3_prev, n4_prev):  # формула расчёта количества выздоровевших людей
-        return n3_prev \
-               + round(self.coefs_dict.get('k2') * n2_prev) \
-               - round(self.coefs_dict.get('k3') * n3_prev) \
-               + round(self.coefs_dict.get('k4') * n4_prev)
+    def calculate_s1(self, s1_prev, h1_prev):  # формула расчёта количества выздоровевших людей
+        return s1_prev \
+               + round(self.coeffs_dict.get('k1') * h1_prev) \
+               - round(self.coeffs_dict.get('k2') * s1_prev) \
+               - round(self.coeffs_dict.get('k6') * s1_prev)
 
-    def n4(self, n3_prev, n4_prev):  # формула расчёта количества повторно заболевших людей
-        return n4_prev \
-               - round(self.coefs_dict.get('k4') * n4_prev) \
-               + round(self.coefs_dict.get('k3') * n3_prev) \
-               - round(self.coefs_dict.get('k6') * n4_prev)
+    def calculate_s2(self, s2_prev, h2_prev):  # формула расчёта количества повторно заболевших людей
+        return s2_prev \
+               + round(self.coeffs_dict.get('k3') * h2_prev) \
+               - round(self.coeffs_dict.get('k4') * s2_prev) \
+               - round(self.coeffs_dict.get('k7') * s2_prev)
 
-    def n5(self, n1_prev, n5_prev):  # формула расчёта количества вакцинировавшихся людей
-        return n5_prev \
-               + round(self.coefs_dict.get('v') * n1_prev) \
-               - round(self.coefs_dict.get('k5') * n5_prev)
-
-    def n6(self, n2_prev, n4_prev, n6_prev):  # формула расчёта количества умерших людей
-        return n6_prev \
-               + round(self.coefs_dict.get('k6') * n2_prev) \
-               + round(self.coefs_dict.get('k6') * n4_prev)
+    def calculate_d(self, d_prev, s1_prev, s2_prev):  # формула расчёта количества вакцинировавшихся людей
+        return d_prev \
+               + round(self.coeffs_dict.get('k6') * s1_prev) \
+               + round(self.coeffs_dict.get('k7') * s2_prev)
 
     def get_disease_stat_array(self, people_distr_list, number_of_days):
         dots_arrays_list = [[people_distr_list[0]],
                             [people_distr_list[1]],
                             [people_distr_list[2]],
                             [people_distr_list[3]],
-                            [people_distr_list[4]],
-                            [people_distr_list[5]]]
+                            [people_distr_list[4]]]
         people_distr_dict = {
-            'n1': people_distr_list[0],  # количество не привитых и не болевших людей
-            'n2': people_distr_list[1],  # количество людей заболевших впервые
-            'n3': people_distr_list[2],  # количество выздоровевших людей
-            'n4': people_distr_list[3],  # количество повторно заболевших людей
-            'n5': people_distr_list[4],  # количество вакцинировавшихся людей
-            'n6': people_distr_list[5],  # количество умерших людей
+            'h1': people_distr_list[0],  # количество непривитых здоровых
+            'h2': people_distr_list[1],  # количество привитых здоровых
+            's1': people_distr_list[2],  # количество непривитых больных
+            's2': people_distr_list[3],  # количество привитых больных
+            'd': people_distr_list[4],   # количество умерших людей
         }
         for day_num in range(number_of_days - 1):
-            n1_new = self.n1(people_distr_dict.get('n1'))
-            n2_new = round(
-                self.n2(people_distr_dict.get('n1'), people_distr_dict.get('n2'), people_distr_dict.get('n5')))
-            n3_new = round(
-                self.n3(people_distr_dict.get('n2'), people_distr_dict.get('n3'), people_distr_dict.get('n4')))
-            n4_new = round(self.n4(people_distr_dict.get('n3'), people_distr_dict.get('n4')))
-            n5_new = round(self.n5(people_distr_dict.get('n1'), people_distr_dict.get('n5')))
-            n6_new = round(
-                self.n6(people_distr_dict.get('n2'), people_distr_dict.get('n4'), people_distr_dict.get('n6')))
-            new_n_list = [n1_new, n2_new, n3_new, n4_new, n5_new, n6_new]
+            h1_new = self.calculate_h1(people_distr_dict.get('h1'), people_distr_dict.get('s1'))
+            h2_new = round(
+                self.calculate_h2(people_distr_dict.get('h1'), people_distr_dict.get('h2'), people_distr_dict.get('s2')))
+            s1_new = round(self.calculate_s1(people_distr_dict.get('s1'), people_distr_dict.get('h1')))
+            s2_new = round(self.calculate_s2(people_distr_dict.get('s2'), people_distr_dict.get('h2')))
+            d_new = round(self.calculate_d(people_distr_dict.get('d'), people_distr_dict.get('s1'), people_distr_dict.get('s1')))
+            new_people_distr_list = [h1_new, h2_new, s1_new, s2_new, d_new]
             k = 0
             for group_name in people_distr_dict:
-                people_distr_dict[group_name] = new_n_list[k]
+                people_distr_dict[group_name] = new_people_distr_list[k]
                 k += 1
             for dots_array_num in range(len(dots_arrays_list)):
-                dots_arrays_list[dots_array_num].append(new_n_list[dots_array_num])
+                dots_arrays_list[dots_array_num].append(new_people_distr_list[dots_array_num])
         return dots_arrays_list
 
-    def check_coefs(self, coefs_list):
+    def check_coeffs(self, coeffs_list):
         pass
